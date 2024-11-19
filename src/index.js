b6cad80b29e4b83fa62f38e832afa50d4d712e35
Ibrahim <i.h.elchami@gmail.com>
2024-11-19 18:01:37 +0300

    Routes and Navbar completed

--- src/index.js
+++ src/index.js
@@ -2,14 +2,51 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import Home from './components/Home'
+import TripsList from './components/TripsList'
+import TripDetail from './components/TripDetail'
+import Nav from './components/Nav';
+
+
 import reportWebVitals from './reportWebVitals';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "/Nav",
+    element: <Nav />,
+  },
+  {
+    path: "/Home",
+    element: <Home />,
+  },
+  {
+    path: "/TripListHome",
+    element: <TripsList />,
+  },
+  {
+    path: "/TripDetail/:tripId",
+    element: <TripDetail />,
+  }
+]);
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
+
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
